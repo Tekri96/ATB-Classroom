c52dac28abf406db88316eddf7beda6d16e90eb1
tekri96 <anandsaurabh17@gmail.com>
2022-07-03 22:06:22 +0530

    Updated Text Box

--- components/welcompage/index.tsx
+++ components/welcompage/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-
+import { motion } from 'framer-motion';
 type Props = {};
 
 export default function WelcomePage() {
   return (
     <div className='w-screen h-screen bg-[#5ABCE6] flex items-center justify-center'>
       <div className='h-full'>
-        <div className='relative flex flex-col justify-center ml-8 -mr-32'>
+        <div className='relative flex flex-col justify-center ml-8 -mr-32 '>
           <div className='absolute translate-x-full translate-y-full'>
-            <div className='absolute top-0 w-64 h-32 translate-x-4 bg-white -translate-y-2/3'>
-              <div className='absolute w-6 h-8 bg-red-400' />
+            <div className='absolute top-0 w-64 p-2 translate-x-4 bg-white -translate-y-2/3'>
+              <div className='absolute bottom-0 flex w-6 h-8 translate-y-full bg-white triangle' />
+              <motion.p
+                animate={{ y: [0, 20, 0] }}
+                className='text-xs font-inter'>
+                Hi! My name is XD. Welcome to the ATB Classroom. I'm so happy to
+                have you here. Let's get started and dive in to all things
+                finance.
+              </motion.p>
+              <div className='w-full text-end'>
+                <button className='px-2 py-1 text-white bg-green-500 rounded-sm text-end'>
+                  Next
+                </button>
+              </div>
             </div>
             <img
               src='/assets/boy.png'
