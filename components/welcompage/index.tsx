8fa74191e585f0ea03c38d82d67ebe9b34eb2e70
tekri96 <anandsaurabh17@gmail.com>
2022-07-07 10:34:36 +0530

    Updated Layout to be Grid

--- components/welcompage/index.tsx
+++ components/welcompage/index.tsx
@@ -7,9 +7,9 @@ export default function WelcomePage() {
   const [page, setPage] = React.useState<PageIdentifier>(PAGES.INTRODUCTION);
   const gotoNextStage = (stage: PageIdentifier) => setPage(stage);
   return (
-    <div className='w-screen h-screen bg-[#5ABCE6] flex items-center justify-center'>
+    <div className='w-screen h-screen pageLayoutGrid bg-[#5ABCE6] overflow-hidden flex items-center justify-center'>
       <div className='h-full'>
-        <div className='relative flex flex-col justify-center ml-8 -mr-32 '>
+        <div className='relative flex flex-col justify-center'>
           <div className='absolute translate-x-full translate-y-full'>
             <div className='absolute top-0 w-64 p-2 translate-x-4 bg-white -translate-y-2/3'>
               <div className='absolute bottom-0 flex w-6 h-8 translate-y-full bg-white triangle' />
@@ -22,11 +22,6 @@ export default function WelcomePage() {
                 have you here. Let's get started and dive in to all things
                 finance.
               </motion.p>
-              {/* <div className='flex justify-between w-full'>
-                <button className='px-2 py-1 text-white bg-green-500 rounded-sm text-end'>
-                  Next
-                </button>
-              </div> */}
             </div>
             <img
               src='/assets/boy.png'
