c3801b9a7866d049c4f843922848c8c3fef1bd4a
tekri96 <anandsaurabh17@gmail.com>
2022-07-08 08:57:11 +0530

    Updated Final Stage

--- components/welcompage/slides/lessonOne/index.tsx
+++ components/welcompage/slides/lessonOne/index.tsx
@@ -31,7 +31,9 @@ export default function UserIntro({ gotoNextStage }: Props) {
         <br />
         <button
           className='bg-[#00FF66] py-2 px-4 rounded-md text-white font-inter'
-          onClick={() => dispatch(updateLessonOneStage(stage + 1))}>
+          onClick={() =>
+            dispatch(updateLessonOneStage(stage + 1 > 2 ? 2 : stage + 1))
+          }>
           Next
         </button>
       </motion.div>
